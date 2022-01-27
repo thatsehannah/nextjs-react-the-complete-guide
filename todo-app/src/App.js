99610cae21db5bb05cdafbe93bf54d95b81f8ff8
E. Hannah <echannah631@gmail.com>
2022-01-26 20:07:38 -0800

    created a todo component, index css

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,7 +1,12 @@
-function App() {
+import Todo from "./components/Todo";
+
+const App = () => {
   return (
-    <div>
-      
+    <div className="body">
+      <h1>My Todos</h1>
+      <Todo title="Learning React" />
+      <Todo title="Mastering React" />
+      <Todo title="Being the best I can be." />
     </div>
   );
 }
