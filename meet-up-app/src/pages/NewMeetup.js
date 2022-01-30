0fc29a3004af3db539a8f233d3709ed7abe77433
Elliot Hannah III <echannah631@gmail.com>
2022-01-29 21:11:02 -0800

    new meetup form

--- meet-up-app/src/pages/NewMeetup.js
+++ meet-up-app/src/pages/NewMeetup.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import NewMeetupForm from '../components/meetups/NewMeetupForm/NewMeetupForm';
 
 const NewMeetupPage = () => {
-  return <div>New Meetup Page</div>;
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm />
+    </section>
+  );
 };
 
 export default NewMeetupPage;
