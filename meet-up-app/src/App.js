ee42fbcfef855009677f2ff79b83d69bfc819018
Elliot Hannah III <echannah631@gmail.com>
2022-01-29 19:14:10 -0800

    routing, 3 pages and navigation

--- meet-up-app/src/App.js
+++ meet-up-app/src/App.js
@@ -1,5 +1,28 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import AllMeetupsPage from './pages/AllMeetups';
+import FavoritesPage from './pages/Favorites';
+import NewMeetupPage from './pages/NewMeetup';
+import MainNavigation from './components/layout/MainNavigation';
+
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <MainNavigation />
+      <Switch>
+        <Route path='/' exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path='/new-meetup'>
+          <NewMeetupPage />
+        </Route>
+        <Route path='/favorites'>
+          <FavoritesPage />
+        </Route>
+      </Switch>
+    </div>
+  );
 };
 
 export default App;
---
+++ meet-up-app/src/pages/AllMeetups.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AllMeetupsPage = () => {
+  return <div>All Meetups Page</div>;
+};
+
+export default AllMeetupsPage;
