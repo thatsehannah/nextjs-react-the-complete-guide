c70d6203c7871277ed92a47d9bab5c89ff868e02
Elliot Hannah III <echannah631@gmail.com>
2022-01-29 20:29:04 -0800

    meetup list & items, css modules

---
+++ meet-up-app/src/components/meetups/MeetupList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import MeetupItem from './MeetupItem';
+import classes from './MeetupList.module.css';
+
+const MeetupList = (props) => {
+  return (
+    <ul className={classes.list}>
+      {props.meetups.map((meetup) => (
+        <MeetupItem key={meetup.id} meetup={meetup} />
+      ))}
+    </ul>
+  );
+};
+
+export default MeetupList;
