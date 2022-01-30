72be1df1b7bdbe5610a1a1b09e91160105f3f437
Elliot Hannah III <echannah631@gmail.com>
2022-01-29 21:38:44 -0800

    useRef, submitHandler

--- meet-up-app/src/components/meetups/NewMeetupForm/NewMeetupForm.js
+++ meet-up-app/src/components/meetups/NewMeetupForm/NewMeetupForm.js
@@ -1,27 +1,50 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 import classes from './NewMeetupForm.module.css';
 import Card from '../../ui/Card/Card';
 
 const NewMeetupForm = () => {
+  const titleInputRef = useRef();
+  const imageInputRef = useRef();
+  const addressInputRef = useRef();
+  const descInputRef = useRef();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const enteredTitle = titleInputRef.current.value;
+    const enteredImage = imageInputRef.current.value;
+    const enteredAddress = addressInputRef.current.value;
+    const entredDesc = descInputRef.current.value;
+
+    const meetupData = {
+      title: enteredTitle,
+      image: enteredImage,
+      address: enteredAddress,
+      description: entredDesc,
+    };
+
+    console.log(meetupData);
+  };
+
   return (
     <Card>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor='title'>Meetup Title</label>
-          <input type='text' required id='title' />
+          <input type='text' required id='title' ref={titleInputRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor='image'>Meetup Image</label>
-          <input type='url' required id='image' />
+          <input type='url' required id='image' ref={imageInputRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor='address'>Address</label>
-          <input type='text' required id='address' />
+          <input type='text' required id='address' ref={addressInputRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor='desc'>Description</label>
-          <textarea id='desc' required rows='5'></textarea>
+          <textarea id='desc' required rows='5' ref={descInputRef}></textarea>
         </div>
         <div className={classes.actions}>
           <button>Add Meetup</button>
