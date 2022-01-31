103687e5b521be96006d9853860bec1740b44ab7
Elliot Hannah III <echannah631@gmail.com>
2022-01-30 23:15:11 -0800

    navigating to home page after creating meetup

--- meet-up-app/src/pages/NewMeetup.js
+++ meet-up-app/src/pages/NewMeetup.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import NewMeetupForm from '../components/meetups/NewMeetupForm/NewMeetupForm';
 
 const NewMeetupPage = () => {
+  const history = useHistory();
+
   const addMeetupHandler = (meetupData) => {
     fetch(
       'https://react-getting-started-5ec2f-default-rtdb.firebaseio.com/meetups.json',
@@ -12,7 +15,9 @@ const NewMeetupPage = () => {
           'Content-Type': 'application/json',
         },
       }
-    );
+    ).then(() => {
+      history.replace('/');
+    });
   };
 
   return (
