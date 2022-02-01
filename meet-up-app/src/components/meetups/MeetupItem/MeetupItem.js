56a48af9e2cff14c663e024ba6a4a38b7dc71139
Elliot Hannah III <echannah631@gmail.com>
2022-02-01 07:51:03 -0800

    using context to add/remove favorite meetups

--- meet-up-app/src/components/meetups/MeetupItem/MeetupItem.js
+++ meet-up-app/src/components/meetups/MeetupItem/MeetupItem.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
 import Card from '../../ui/Card/Card';
 import classes from './MeetupItem.module.css';
+import FavoritesContext from '../../../store/favorites-context';
 
 const MeetupItem = (props) => {
+  const favoritesCtx = useContext(FavoritesContext);
+  const itemIsFavorite = favoritesCtx.itemIsFavorite(props.meetup.id);
+
+  function toggleFavoriteStatusHandler() {
+    if (itemIsFavorite) {
+      favoritesCtx.removeFavorite(props.meetup.id);
+    } else {
+      favoritesCtx.addFavorite({
+        id: props.meetup.id,
+        title: props.meetup.title,
+        description: props.meetup.description,
+        image: props.meetup.image,
+        address: props.meetup.address,
+      });
+    }
+  }
+
   return (
     <li className={classes.item}>
       <Card>
@@ -15,7 +34,9 @@ const MeetupItem = (props) => {
           <p>{props.meetup.description}</p>
         </div>
         <div className={classes.actions}>
-          <button>To Favorites</button>
+          <button onClick={toggleFavoriteStatusHandler}>
+            {itemIsFavorite ? 'Remove from Favorites' : 'To Favorites'}
+          </button>
         </div>
       </Card>
     </li>
