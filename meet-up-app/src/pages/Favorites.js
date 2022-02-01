409a2e93a4d109bf3c750edea230273c3b92a3e3
Elliot Hannah III <echannah631@gmail.com>
2022-02-01 08:17:58 -0800

    using fav context on fav page & nav

--- meet-up-app/src/pages/Favorites.js
+++ meet-up-app/src/pages/Favorites.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import MeetupList from '../components/meetups/MeetupList/MeetupList';
+
+import FavoritesContext from '../store/favorites-context';
 
 const FavoritesPage = () => {
-  return <div>Favorites Page</div>;
+  const favoriteCtx = useContext(FavoritesContext);
+
+  let content;
+
+  if (favoriteCtx.totalFavorites === 0) {
+    content = <p>You do not have any favorites yet. Start adding some?</p>;
+  } else {
+    content = <MeetupList meetups={favoriteCtx.favorites} />;
+  }
+
+  return (
+    <section>
+      <h1>My Favorite Meetups</h1>
+      {content}
+    </section>
+  );
 };
 
 export default FavoritesPage;
