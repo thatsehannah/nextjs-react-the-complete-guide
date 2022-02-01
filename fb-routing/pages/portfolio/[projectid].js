d3bff6499173efb1c71ccdae683aeeac573cc070
Elliot Hannah III <echannah631@gmail.com>
2022-02-01 09:13:11 -0800

    [fb-routing] dynamic routing

---
+++ fb-routing/pages/portfolio/[projectid].js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+
+const PortfolioProjectPage = () => {
+  const router = useRouter();
+
+  console.log(router.pathname); //{}
+  console.log(router.query); //[projectid: 1]
+
+  return (
+    <div>
+      <h1>The Portfolio Project Page</h1>
+    </div>
+  );
+};
+
+export default PortfolioProjectPage;
