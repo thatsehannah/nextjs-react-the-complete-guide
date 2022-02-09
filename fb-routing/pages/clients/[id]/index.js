a25430bde0ecad3d3083039a45d2e9d58946e695
Elliot Hannah III <echannah631@gmail.com>
2022-02-09 14:29:35 -0800

    [fb-routing] dynamic navigation

--- fb-routing/pages/clients/[id]/index.js
+++ fb-routing/pages/clients/[id]/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import { useRouter } from 'next/router';
+import { Router, useRouter } from 'next/router';
 
 //URL
 //domain.com/clients/<id>
 const ClientProjects = () => {
-  console.log(useRouter().query);
+  const router = useRouter();
+  console.log(router.query);
+
+  const loadProjectHandler = () => {
+    router.push({
+      pathname: '/clients/[id]/[clientprojectid]',
+      query: { id: 'elliot', clientprojectid: 'projecta' },
+    });
+  };
 
   return (
     <div>
       <h1>The Projects of a Given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 };
--- fb-routing/pages/clients/index.js
+++ fb-routing/pages/clients/index.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import Link from 'next/link';
 
 //URL
 //domain.com/clients
 const Clients = () => {
+  const clients = [
+    { id: 'elliot', name: 'Elliot' },
+    { id: 'wilma', name: 'Wilma' },
+  ];
+
   return (
     <div>
       <h1>The Clients Page</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link
+              href={{
+                pathname: '/clients/[id]',
+                query: { id: client.id },
+              }}
+            >
+              {client.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
