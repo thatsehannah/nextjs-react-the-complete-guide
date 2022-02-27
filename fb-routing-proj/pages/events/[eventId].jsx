7a7597ba447af7fbca3bd5151e683349854b7cb8
Elliot Hannah III <echannah631@gmail.com>
2022-02-27 14:37:37 -0800

    [fb-routing-proj] fixed broken import

--- fb-routing-proj/pages/events/[eventId].jsx
+++ fb-routing-proj/pages/events/[eventId].jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 import { getEventById } from '../../dummy-data';
-import EventSummary from '../../components/event-detail/EventContent';
+import EventSummary from '../../components/event-detail/EventSummary';
 import EventLogistics from '../../components/event-detail/EventLogistics';
 import EventContent from '../../components/event-detail/EventContent';
 import ErrorAlert from '../../components/ui/ErrorAlert';
